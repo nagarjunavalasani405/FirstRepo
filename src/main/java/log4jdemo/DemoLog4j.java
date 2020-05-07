63e966a681c67a0e76be1c8f7fd91293e257e76d
valasani <nagarjuna.valasani405@gmail.com>
2020-05-07 13:23:14 +0530

    hiii

--- src/main/java/log4jdemo/DemoLog4j.java
+++ src/main/java/log4jdemo/DemoLog4j.java
@@ -29,7 +29,7 @@ public class DemoLog4j {
 
 
 		    System.out.println("this is example of log4j");
-		    System.out.println("hi this example of log4j");
+		    System.out.println("hiii this example of log4j");
 		  
 		  
 		  logs.info("this is information message");
