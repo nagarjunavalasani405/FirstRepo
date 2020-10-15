9dc1a0a0a36840b9d359a2d2e8bb0a91482b6016
valasani <nagarjuna.valasani405@gmail.com>
2020-10-15 15:04:16 +0530

    this is changed commit

--- src/main/java/log4jdemo/DemoLog4j.java
+++ src/main/java/log4jdemo/DemoLog4j.java
@@ -1,8 +1,7 @@
 package log4jdemo;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -11,16 +10,15 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class DemoLog4j {
-	WebDriver driver;
 	
-      static Logger logs=LogManager.getLogger(DemoLog4j.class);
-     
+	WebDriver driver;
+        
         @BeforeMethod
         
         public void setup() {
 
-    		System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
-    	    driver=new ChromeDriver();
+        	System.setProperty("webdriver.chrome.driver", "D:\\newchrome\\chromedriver.exe");
+	        driver = new ChromeDriver();	
     	    driver.manage().deleteAllCookies();
     	    driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
     	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -31,17 +29,17 @@ public class DemoLog4j {
 		    System.out.println("hiii helo this example of log4j");
 		  
 		  
-		  logs.info("this is information message");
-		  logs.error("this is error message");
-		  logs.debug("this is debug messge");
-		  logs.fatal("this is fatal message");
+		 // logs.info("this is information message");
+		  //logs.error("this is error message");
+		  //logs.debug("this is debug messge");
+		  //logs.fatal("this is fatal message");
 		  }
         @Test
         public void TestCase() {
         	
-        	String title = driver.getTitle();
-        	System.out.println("the current tittle is:"+title);
-        	Assert.assertEquals(title, "OrangeHRM");
+           String tittle =  driver.getTitle();
+        	System.out.println("the current tittle is:" + tittle);
+        	Assert.assertEquals(tittle, "OrangeHRM");
         	System.out.println("test completed");
          }
         
