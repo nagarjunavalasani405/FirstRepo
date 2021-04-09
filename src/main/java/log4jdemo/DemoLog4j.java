0bff8af97eb819e25b18d74a18fb86c94ee95418
valasani <nagarjuna.valasani405@gmail.com>
2021-04-09 21:55:08 +0530

    latest commit by me

--- src/main/java/log4jdemo/DemoLog4j.java
+++ src/main/java/log4jdemo/DemoLog4j.java
@@ -1,7 +1,8 @@
 package log4jdemo;
+
 import java.util.concurrent.TimeUnit;
 
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -12,22 +13,24 @@ import org.testng.annotations.Test;
 public class DemoLog4j {
 	
 	WebDriver driver;
-        
+	
+	private static Logger Log = Logger.getLogger(Logger.class.getName());
+	
         @BeforeMethod
         
         public void setup() {
 
-        	System.setProperty("webdriver.chrome.driver", "D:\\Chrome86\\chromedriver.exe");
+        	System.setProperty("webdriver.chrome.driver", "D:\\Chrome88\\chromedriver.exe");
 	        driver = new ChromeDriver();	
     	    driver.manage().deleteAllCookies();
     	    driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
     	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     	    driver.get("https://opensource-demo.orangehrmlive.com/index.php/admin/viewSystemUsers");
 
-
+            
 		    System.out.println("this is example of log4j");
 		  
-		 // logs.info("this is information message");
+		    Log.info("this is information message");
 		  //logs.error("this is error message");
 		  //logs.debug("this is debug messge");
 		  //logs.fatal("this is fatal message");
